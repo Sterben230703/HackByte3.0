be82658152c056d3d43f7940ee4657832c5e3122
Anand Jaiswal <anand230703@gmail.com>
2025-04-05 17:08:01 +0530

    project setup modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-})
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    build: {
+      outDir: "dist",
+    },
+    server: {
+      open: true,
+    },
+    plugins: [
+      react()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./frontend"),
+      },
+    },
+    define: {
+      'process.env': env
+    }
+  }
+});
